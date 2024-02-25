62097e5e6c4f9a479f663b8aaf9c2f091713d3d3
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-25 22:23:55 +0100

    setup inventories entity and repository

---
+++ src/main/java/com/inventory/inventory/inventories/InventoryRepository.java
@@ -0,0 +1,6 @@
+package com.inventory.inventory.inventories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
+}
--- src/main/java/com/inventory/inventory/products/Product.java
+++ src/main/java/com/inventory/inventory/products/Product.java
@@ -20,5 +20,6 @@ public class Product {
     private int quantity;
     private String name;
     private double price;
+    private String description;
 
 }
