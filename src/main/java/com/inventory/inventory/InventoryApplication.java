e69e320cf3f9730bae8caf7211cd1034e2249757
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-27 23:33:27 +0100

    fix auto config application & repository error

--- src/main/java/com/inventory/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/inventory/InventoryApplication.java
@@ -1,9 +1,22 @@
 package com.inventory.inventory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+
+})
+@EnableJpaRepositories
+
 public class InventoryApplication {
 
 	public static void main(String[] args) {
